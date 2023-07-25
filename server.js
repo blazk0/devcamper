e8c13eb3f7d54b11b44ae8dfc6909ea9572cb7a7
Amr Mostafa <amr.abouelanin@musala.com>
2023-07-25 14:39:01 +0300

    feat: section 4

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Router Files
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
