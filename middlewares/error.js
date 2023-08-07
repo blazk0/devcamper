2553d804bdacec14f0c5405384bf50c24b6c74dc
Amr Mostafa <amr.abouelanin@musala.com>
2023-08-07 15:34:11 +0300

    feat: section 5

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,34 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
   console.log(err.stack.red);
 
-  res.status(500).json({ success: false, error: err.message });
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error',
+  });
 };
 
 module.exports = errorHandler;
