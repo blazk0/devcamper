17fd006e64cbe84210f1c03ed66fa08f2de09009
Amr Mostafa <amr.abouelanin@musala.com>
2023-08-11 15:05:31 +0300

    feat: finish section 6

---
+++ seeder.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
+
+mongoose.connect(process.env.MONGO_URI);
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+
+    console.log('Data imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
