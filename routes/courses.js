c0a23187086ec0ab02867845f776187a0e7116cb
Amr Mostafa <amr.abouelanin@musala.com>
2023-08-14 16:55:53 +0300

    feat: section 7

--- routes/courses.js
+++ routes/courses.js
@@ -12,6 +12,8 @@ const advancedResults = require('../middlewares/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect, authorize } = require('../middlewares/auth');
+
 router
   .route('/')
   .get(
@@ -21,7 +23,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
